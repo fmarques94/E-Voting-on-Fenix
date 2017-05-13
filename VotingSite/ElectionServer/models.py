89b48f2a5656d999e2c4b64574680a7832a1baf3
Fernando Marques <fernando.m.marques@ist.utl.pt>
2017-05-13 16:21:44 +0100

    Added new models and base and home html pages. Also added base css

--- VotingSite/ElectionServer/models.py
+++ VotingSite/ElectionServer/models.py
@@ -5,3 +5,17 @@ class FenixUser(AbstractUser):
     name = models.CharField(max_length=250)
     email = models.EmailField(max_length=100)
     status = models.CharField(max_length=100)
+
+class Election(models.Model):
+    uuid = models.CharField(max_length=36, primary_key=True)
+    name = models.CharField(max_length=400)
+    description = models.TextField()
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    timeOpenBooth = models.TimeField()
+    timeCloseBooth = models.TimeField()
+    admin = models.ForeignKey(FenixUser, on_delete = models.CASCADE)
+
+class Trustee(models.Model):
+    election = models.ForeignKey(Election, on_delete = models.CASCADE)
+    user = models.ForeignKey(FenixUser,on_delete=models.CASCADE)
--- VotingSite/ElectionServer/views.py
+++ VotingSite/ElectionServer/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
+from ElectionServer.models import Election
+
 # Create your views here.
 
-@login_required
+#@login_required
 def home(request):
-    return render(request,'home.html')
+    context = {'Elections': Election.objects.all()}
+    return render(request,'home.html',context)
 
 def login(request):
     return render(request,'login.html')
