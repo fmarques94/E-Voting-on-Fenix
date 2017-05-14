cad55ec648005d1a76292b0850b278950da9592c
Fernando Marques <fernando.m.marques@ist.utl.pt>
2017-05-14 16:28:46 +0100

    Added form to create election

--- VotingSite/ElectionServer/urls.py
+++ VotingSite/ElectionServer/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^login/$', views.login, name='login'),
+    url(r'^create/$',views.createElection, name='create'),
+    url(r'^election/$',views.election,name='election'),
+    #url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout',kwargs={'next_page': '/'}),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
--- VotingSite/ElectionServer/views.py
+++ VotingSite/ElectionServer/views.py
@@ -1,16 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
 from ElectionServer.models import Election
+from ElectionServer.forms import ElectionCreationForm
+import uuid
 
 # Create your views here.
 
 #@login_required
 def home(request):
-    context = {'Elections': Election.objects.all()}
+    context = {'elections': Election.objects.all()}
     return render(request,'home.html',context)
 
-def login(request):
-    return render(request,'login.html')
-    
+
+def createElection(request):
+    if request.method == 'POST':
+        form = ElectionCreationForm(request.POST)
+        if form.is_valid():
+            newElection = Election()
+            newElection.uuid = uuid.uuid4()
+            newElection.name = form.cleaned_data['name']
+            newElection.description = form.cleaned_data['description']
+            newElection.start = form.cleaned_data['startTime']
+            newElection.end = form.cleaned_data['endTime']
+            newElection.timeOpenBooth = form.cleaned_data['timeOpenBooth']
+            newElection.timeCloseBooth = form.cleaned_data['timeCloseBooth']
+            newElection.admin = request.user
+            newElection.save()
+            return redirect(election)
+    else:
+        form = ElectionCreationForm()
+        print(form)
+        return render(request,'create.html',{'form':form})
+
+def election(request):
+    return render(request,'election.html')
