6da9a1518a9b82c2f077facbe1d6b2e28a83e624
Fernando Marques <fernandomarques94@live.com.pt>
2017-07-11 15:45:56 +0100

    Finished Rest Server part of backend.

--- VotingSite/ElectionServer/views.py
+++ VotingSite/ElectionServer/views.py
@@ -1,3 +1,11 @@
+#Core
+import json
+import requests
+import csv
+import requests
+import datetime
+
+#Django
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
@@ -5,6 +13,7 @@ from django.core import serializers
 from django.http import HttpResponse, HttpResponseNotAllowed, Http404, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.db import transaction
+from django.conf import settings
 
 #Models
 from ElectionServer.models import Election 
@@ -12,6 +21,7 @@ from ElectionServer.models import Trustee
 from ElectionServer.models import Voter
 from ElectionServer.models import Question
 from ElectionServer.models import Answer
+from ElectionServer.models import Ballot
 
 def home(request):
     context = {'elections': Election.objects.all()}
@@ -77,6 +87,8 @@ def election(request,election_id):
     else:
         return HttpResponseNotAllowed(['GET'])
 
+#########################################################################################################################
+
 @login_required
 @csrf_exempt
 def addTrustees(request,election_id):
@@ -85,6 +97,9 @@ def addTrustees(request,election_id):
             election = Election.objects.get(id=election_id)
             if request.user != election.admin:
                 return HttpResponseForbidden("Access denied")
+            if datetime.now() >= election.startDate:
+            return HttpResponse(json.dumps({'error':'The election has already started. Cannot add trustees now.'}),
+             content_type='application/json', status=404)
             data = json.loads(request.body.decode('utf-8'))
             with transaction.atomic():
                 for trusteeData in data['trusteeList']:
@@ -96,10 +111,14 @@ def addTrustees(request,election_id):
                     trustee.save()
             except Election.DoesNotExist:
                 return HttpResponse(json.dumps({'error':'Election does not exist'}), content_type='application/json', status=404)
+            except IntegrityError:
+                return HttpResponse(json.dumps({'error':'Trustee '+ trusteeData['id'] + ' already present'})
+                , content_type='application/json', status=404)
             except Exception as exception:
                 return HttpResponse(json.dumps({'error':repr(exception)}), content_type='application/json')
     else:
         return HttpResponseNotAllowed(['POST'])
+
 
 @login_required
 @csrf_exempt
@@ -121,6 +140,20 @@ def removeTrustees(request,election_id):
                 return HttpResponse(json.dumps({'error':repr(exception)}), content_type='application/json')  
     else:
         return HttpResponseNotAllowed(['POST'])
+
+@login_required
+def getTrustees(request,election_id):
+    if request.method == 'GET':
+        try:
+            election = Election.objects.get(id=election_id)
+        except Election.DoesNotExist:
+            return HttpResponse(json.dumps({'error':'Election does not exist'}), content_type='application/json', status=404)
+        data = serializers.serialize('json',list(Trustee.objects.all().filter(election=election)))
+        return HttpResponse(json.dumps(data),content_type='application/json')
+    else:
+        return HttpResponseNotAllowed(['GET'])
+
+######################################################################################################################
 
 @login_required
 def addVoters(request,election_id):
@@ -175,6 +208,9 @@ def addVoters(request,election_id):
             return HttpResponse(json.dumps({'error':'Election does not exist'}), content_type='application/json', status=404)
         if request.user != election.admin:
             return HttpResponseForbidden("Access denied")
+        if datetime.now() >= election.startDate:
+            return HttpResponse(json.dumps({'error':'The election has already started. Cannot add voters now.'}),
+             content_type='application/json', status=404)
         if request.FILES:
             return addVotersFile(request,election)
         else:
@@ -205,8 +241,22 @@ def removeVoters(request,election_id):
         return HttpResponseNotAllowed(['POST'])
 
 @login_required
+def getVoters(request,election_id):
+    if request.method == 'GET':
+        try:
+            election = Election.objects.get(id=election_id)
+        except Election.DoesNotExist:
+            return HttpResponse(json.dumps({'error':'Election does not exist'}), content_type='application/json', status=404)
+        data = serializers.serialize('json',list(Voter.objects.all().filter(election=election)))
+        return HttpResponse(json.dumps(data),content_type='application/json')
+    else:
+        return HttpResponseNotAllowed(['GET'])
+
+####################################################################################################################
+
+@login_required
 @csrf_exempt
-def addQuestion(request,election_id):
+def addQuestions(request,election_id):
     if request.method == 'POST':
         try:
             election = Election.objects.get(id=election_id)
@@ -214,6 +264,9 @@ def addQuestion(request,election_id):
             return HttpResponse(json.dumps({'error':'Election does not exist'}), content_type='application/json', status=404)
         if request.user != election.admin:
             return HttpResponseForbidden("Access denied")
+        if datetime.now() >= election.startDate:
+            return HttpResponse(json.dumps({'error':'The election has already started. Cannot add questions now.'}),
+             content_type='application/json', status=404)
         data = json.loads(request.body.decode('utf-8'))
         try:
             with transaction.atomic():
@@ -232,6 +285,115 @@ def addQuestion(request,election_id):
     else:
         return HttpResponseNotAllowed(['POST'])
 
+@login_required
+@csrf_exempt
+def removeQuestions(request,election_id):
+    if request.method == 'POST':
+        try:
+            election = Election.objects.get(id=election_id)
+        except Election.DoesNotExist:
+            return HttpResponse(json.dumps({'error':'Election does not exist'}), content_type='application/json', status=404)
+        if request.user != election.admin:
+            return HttpResponseForbidden("Access denied")
+        data = json.loads(request.body.decode('utf-8'))
+        try:
+            for questionData in data['questionList']:
+                Question.objects.get(id=questionData['id'],election=election).delete()
+            except Question.DoesNotExist:
+                pass
+        except Exception as exception:
+            return HttpResponse(json.dumps({'error':repr(exception)}), content_type='application/json')
+        return HttpResponse(json.dumps({'success':True}), content_type='application/json')      
+    else:
+        return HttpResponseNotAllowed(['POST'])
+
+@login_required
+def getQuestions(request,election_id):
+    if request.method == 'GET':
+        try:
+            election = Election.objects.get(id=election_id)
+        except Election.DoesNotExist:
+            return HttpResponse(json.dumps({'error':'Election does not exist'}), content_type='application/json', status=404)
+        data = serializers.serialize('json',list(Question.objects.all().filter(election=election)))
+        return HttpResponse(json.dumps(data),content_type='application/json')
+    else:
+        return HttpResponseNotAllowed(['GET'])
+
+#############################################################################################################################
+@login_required
+def register(request,election_id):
+    if request.method == 'GET':
+        try:
+            election = Election.objects.get(id=election_id)
+        except Election.DoesNotExist:
+            return HttpResponse(json.dumps({'error':'Election does not exist'}), content_type='application/json', status=404)
+        try:
+            voter = Voter.objects.get(id=request.user,election=election)
+        except Voter.DoesNotExist:
+            return HttpResponse(json.dumps({'error':'Voter is not eligible for this election'}),
+             content_type='application/json', status=404)
+        payload = {
+            'election':election.id,
+            'cryptoParameters':election.cryptoParameters,
+            'voter':voter.id,
+            'email':voter.email,
+        }
+        r = requests.json(settings.CREDENTIAL_AUTHORITY,json=payload)
+        if r.status!=200:
+            return HttpResponse(json.dumps({'error':r.status}),content_type='application/json')
+        data = json.loads(r.json())
+        try:
+            voter.publicCredential = data['publicCredential']
+            voter.save()
+        except Exception as exception:
+            return HttpResponse(json.dumps({'error':repr(exception)}), content_type='application/json')
+    else:
+        return HttpResponseNotAllowed(['GET'])
+
+##############################################################################################################################
+@login_required
+@csrf_exempt
+def cast(request,election_id):
+    if request.method == 'GET':
+        try:
+            election = Election.objects.get(id=election_id)
+        except Election.DoesNotExist:
+            return HttpResponse(json.dumps({'error':'Election does not exist'}), content_type='application/json', status=404)
+        try:
+            voter = Voter.objects.get(id=request.user,election=election)
+        except Voter.DoesNotExist:
+            return HttpResponse(json.dumps({'error':'Voter is not eligible for this election'}),
+             content_type='application/json', status=404)
+        if datetime.now() <= election.startDate:
+            return HttpResponse(json.dumps({'error':'The election has not started'}),
+             content_type='application/json', status=404)
+        if datetime.now() >= election.endDate:
+            return HttpResponse(json.dumps({'error':'The election has ended'}),
+             content_type='application/json', status=404)
+        if election.openCastTime!=None and election.closeCastTime!=None:
+            if datetime.now().time() <= election.openCastTime and datetime.now().time() >= election.closeCastTime:
+                return HttpResponse(json.dumps({'error':'The ballot box is closed between ' + election.openCastTime + ' and '
+                + election.closeCastTime}),
+             content_type='application/json', status=404)
+        data = json.loads(request.body.decode('utf-8'))
+        try:
+            if voter.publicCredential!=data['publicCredential']:
+                return HttpResponse(json.dumps({'error':'Invalid public credential for voter with id ' + voter.id}),
+             content_type='application/json', status=404)
+             ballot = Ballot.objects.get(election=election,publicCredential=voter.publicCredential)
+             return HttpResponse(json.dumps({'error':'The voter with id ' + voter.id + ' has already cast a ballot'}),
+             content_type='application/json', status=404)
+        except Ballot.DoesNotExist:
+            #TODO verify ZK-proofs
+            try:
+                ballot = Ballot()
+                ballot.publicCredential = voter.publicCredential
+                ballot.ballot = data['ballot']
+                ballot.save()
+        except Exception as exception:
+            return HttpResponse(json.dumps({'error':repr(exception)}), content_type='application/json')
+    else:
+        return HttpResponseNotAllowed(['GET'])
 
 '''from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
